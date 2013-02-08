f8abbe82815e7e3470ef95b853108a126bff34ed
Jared Sealey <jsealey762@gmail.com>
2013-02-07 20:46:17 -0800

    fix some issues with the database not initializing correctly

--- Delightful/AppDelegate.m
+++ Delightful/AppDelegate.m
@@ -82,7 +82,7 @@
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Delightful" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Delightful" withExtension:@"mom"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
